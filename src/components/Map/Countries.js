d25cf45e39822ef82946ca9386840380c5c24434
Yu Lin <linyu3294@gmail.com>
2020-04-05 13:08:54 -0400

    Added markers and improved zoom but still buggy

--- src/components/Map/Countries.js
+++ src/components/Map/Countries.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { geoPath } from "d3-geo"
-import {Geography, Marker} from "react-simple-maps";
+import {Geography, Marker, ZoomableGroup} from "react-simple-maps";
 
 
 //= ({geo, iter, projection, switchPaths, colors})
@@ -10,17 +10,6 @@ const markers = [
         name: "Buenos Aires",
         coordinates: [-58.3816, -34.6037]
     },
-    { markerOffset: -15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
-    { markerOffset: 25, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
-    { markerOffset: 25, name: "Santiago", coordinates: [-70.6693, -33.4489] },
-    { markerOffset: 40, name: "Bogota", coordinates: [-74.0721, 4.711] },
-    { markerOffset: 25, name: "Quito", coordinates: [-78.4678, -0.1807] },
-    { markerOffset: -15, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
-    { markerOffset: -15, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
-    { markerOffset: 25, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
-    { markerOffset: 25, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
-    { markerOffset: -15, name: "Caracas", coordinates: [-66.9036, 10.4806] },
-    { markerOffset: -15, name: "Lima", coordinates: [-77.0428, -12.0464] }
 ];
 
 class  Countries extends React.Component {
@@ -38,11 +27,6 @@ class  Countries extends React.Component {
     render() {
         return (
             <Geography
-                key={
-                    (this.props.geo.properties.ISO_A3 || this.props.geo.properties.GID_1)
-                    + this.props.iter
-                }
-
                 geography={this.props.geo}
                 // projection={this.props.projection}
 
@@ -64,11 +48,9 @@ class  Countries extends React.Component {
                     }
                 }}
             >
-
             </Geography>
+
         )
-        {/*</div>*/
-        }
     }
 }
 export default Countries
--- src/components/Map/MapChart.js
+++ src/components/Map/MapChart.js
@@ -1,26 +1,21 @@
 import React, {Component} from "react"
-import {ComposableMap, Geographies, Geography, Marker, ZoomableGroup} from "react-simple-maps"
-import {useSpring, animated} from 'react-spring'
+import {ComposableMap, Geographies, Marker, ZoomableGroup} from "react-simple-maps"
 import {Spring} from 'react-spring/renderprops'
 import chroma from "chroma-js"
 import Countries from "./Countries";
-import projection from "d3-geo/src/projection";
 import {geoTimes} from "d3-geo-projection";
 import {geoPath} from "d3-geo";
 import "../component.style.css"
 
+//World Map Json File
+const world = require("./JsonFiles/world");
+const cities = require("./JsonFiles/cities")
 
-//useZoomPan
-const geoUrl =
-    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
-    // "/world.json"
 
+//Randomized color scheme that will casted to each country
+const colorScale = chroma.brewer.Pastel2.slice(1)
 const getRandomInt = (min, max) =>
     Math.floor(Math.random() * (max - min + 1) + min)
-
-
-const colorScale = chroma.brewer.Pastel2.slice(1)
-
 const colors = Array(180)
     .fill()
     .map(d => colorScale[getRandomInt(0, colorScale.length - 1)])
@@ -35,7 +30,7 @@ class MapChart extends Component {
             width: 500,
             height: 700,
             detail: false,
-            paths: geoUrl,
+            paths: world,
             center: [0,0],
             zoom: 1
         }
@@ -53,73 +48,75 @@ class MapChart extends Component {
     // }
     switchPaths = (geo, projection, evt) => {
         // alert(geo.properties.ISO_A3)
+
         const gp = geoPath().projection(projection)
         const dim = evt.target.getBoundingClientRect()
-        console.log("dim: ", dim)
-        const cx = evt.clientX
-        console.log("cx: ", cx)
-        const cy = evt.clientY
-        console.log("cy: ", cy)
+        const cx = evt.clientX - dim.left
+        const cy = evt.clientY - dim.top
         const [orgX, orgY] = gp.bounds(geo)[0]
-        console.log("[orgX, orgY] : ", [orgX, orgY])
-        const centeroid = projection.invert([orgX , orgY])
-        console.log(centeroid)
+        const centeroid = this.projection().invert([orgX + cx, orgY + cy])
+
+
         const { detail } = this.state;
         this.setState(prevState => ({
-            paths: detail ? geoUrl : '/ch.json',
+            ...prevState,
+            paths: detail ? world :this.state.paths,
             center: detail? [0,0]: centeroid,
-            zoom: detail ? 1 : 10,
+            zoom: detail ? 1 : 6,
             detail: !detail
         }));
     };
 
     render() {
+
         return(
+
             <Spring
-                from={{ zoom: 1 }}
+                from={{ zoom: 2 }}
                 to={{ zoom: this.state.zoom }}
 
             >
                 {styles =>(
-                    <div>
+                    <div ref={wrapper => this._wrapper = wrapper} className= "container">
                         {/*<animated.div style={zoomIn}>I will fade in</animated.div>*/}
-                        <ComposableMap       width={this.props.width}
+                        <ComposableMap
+                                             width={this.props.width}
                                              height={this.props.height}
                                              projection={this.projection()}
                                              // className="ratio-container-content ratio-container"
                         >
                             <ZoomableGroup   center={this.state.center}
                                              zoom={styles.zoom}>
-                                <Geographies geography={geoUrl}>
-                                    {({geographies, projection}) =>
+                                <Geographies geography={this.state.paths}>
+                                    {
+                                        ({geographies, projection}) =>
                                         geographies.map((geo,iter) =>
                                             <Countries
+                                                key = {geo.properties.ISO_A3 + iter}
                                                 geo = {geo}
                                                 iter ={iter}
                                                 projection = {projection}
                                                 colors = {colors}
                                                 switchPaths={this.switchPaths}
                                             />
-                                        )}
+                                        )
+                                    }
                                 </Geographies>
-                                <Marker key="This" coordinates={[-60,10]}>
-                                    <circle b={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-                                    <text
-                                        textAnchor="middle"
-                                        style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-                                    >
-                                        {[-60 , 10]}
-                                    </text>
-                                </Marker>
-                                <Marker key="This" coordinates={this.state.center}>
-                                    <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-                                    <text
-                                        textAnchor="middle"
-                                        style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
-                                    >
-                                        {this.state.center}
-                                    </text>
-                                </Marker>
+                                {this.state.detail === true &&
+                                cities.map(city =>
+                                    <Marker key="That" coordinates={city.coordinates}>
+                                        <circle r={5} fill="#F00" stroke="#fff" strokeWidth={2}/>
+                                        <text
+                                            textAnchor="middle"
+                                            strokeWidth = {2}
+                                            style={{fontFamily: "system-ui", fill: "#5D5A6D"}}
+                                        >
+                                            {city.coordinates}
+                                        </text>
+                                    </Marker>
+                                    )
+                                }
+
                             </ZoomableGroup>
                         </ComposableMap>
                     </div>
